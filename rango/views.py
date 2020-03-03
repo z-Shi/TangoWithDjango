7f1777faf3631c542d23db7305ab50b67427a329
z-Shi <zshi767@gmail.com>
2020-03-03 15:46:03 +0000

    Tango w/ Django Files -> Almost Ch. 5 Completed

---
+++ rango/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def index(request):
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
+
+def about(request):
+    context_dict = {'boldmessage': 'This tutorial has been put together by Kathleen.'}
+    return render(request, 'rango/about.html', context=context_dict)
