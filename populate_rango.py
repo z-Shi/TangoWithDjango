47f4715e766cf58af42eda39862079e4d3dd2662
Kathleen West <2509480w@student.gla.ac.uk>
2020-03-30 19:55:27 +0100

    Adding Extra Data to Population Script

--- populate_rango.py
+++ populate_rango.py
@@ -44,7 +44,13 @@ def populate():
     cats = {
         'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
         'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
-        'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16}
+        'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16},
+        'Pascal': {'pages': [], 'views': 0, 'likes': 0},
+        'Perl': {'pages': [], 'views': 0, 'likes': 0},
+        'PHP': {'pages': [], 'views': 0, 'likes': 0},
+        'Prolog': {'pages': [], 'views': 0, 'likes': 0},
+        'PostScript': {'pages': [], 'views': 0, 'likes': 0},
+        'Programming': {'pages': [], 'views': 0, 'likes': 0},
     }
 
     for cat, cat_data in cats.items():
